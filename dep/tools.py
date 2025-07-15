aa60b2283082f29726e738f91da1ec6e6d375def
Alexander Gresch <alexander.gresch@hhu.de>
2025-07-15 09:52:47 +0200

    retrieved left-over code from previous iteration

--- dep/tools.py
+++ dep/tools.py
@@ -25,3 +25,12 @@ class AdamOptimizer:
     
     def get_momentum(self):
         return self.m
+    
+def num_after_point(x):
+    s = str(x)
+    if not '.' in s:
+        return 0
+    return len(s) - s.index('.') - 1
+
+def trunc(values, decs=0):
+    return np.trunc(values*10**decs)/(10**decs)
